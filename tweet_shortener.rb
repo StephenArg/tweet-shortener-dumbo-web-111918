3166eb0b3d12fbbe5f03ad2c8de50c301c917cdf
Stephen Argentina <stephena9871@gmail.com>
2018-11-11 21:43:34 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def word_substituter(word)
+  dictionary = {"hello"=>'hi',"to"=>'2',"two"=>'2',"too"=>'2',"for"=>'4',"four"=>'4',
+  "be"=>'b',"you"=>'u',"at"=>'@',"and"=>'&'}
+  if dictionary.include? (word)
+    return dictionary[word]
+  end
+end
+
+def bulk_tweet_shortener(array)
+  array.each {|tweet|
+  tweet = selective_tweet_shortener(tweet)
+  tweet = shortened_tweet_truncator(tweet)
+  puts tweet}
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    tweet.each {|word|
+    word = word_substituter(word)}
+    tweet
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+    return (tweet[0..137] + "...")
+  else
+    return tweet
+  end
+end
